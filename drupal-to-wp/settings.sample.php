d54717a7fed6d22d073db2d3a897ee5ec496fda9
ddean4040 <ddean@njstatelib.org>
2014-02-21 14:54:08 -0500

    Replace settings file with sample settings file.

---
+++ drupal-to-wp/settings.sample.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Constants for accessing the Drupal database
+ */
+define( 'DR_HOST', 'localhost' );
+
+if( isset( $_REQUEST['drupal_db'] ) ) {
+	define( 'DR_DB', $_REQUEST['drupal_db'] );
+} else {
+	define( 'DR_DB', 'drupal' );
+}
+define( 'DR_USER', 'username' );
+define( 'DR_PASS', 'password' );
+
+/**
+ * Path to your WordPress install
+ */
+define( 'WP_PATH', 'wp' );
+
+/**
+ * Constants for accessing the WordPress database -- only needed if erasing a WP site
+ */
+define( 'WP_HOST', 'localhost' );
+define( 'WP_DB',   'wordpress' );
+define( 'WP_USER', 'username' );
+define( 'WP_PASS', 'password' );
+
+?>
